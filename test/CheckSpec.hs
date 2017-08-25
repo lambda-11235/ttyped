0eb6cbdbf77e143eac9cb58c94d36d0048b74f08
Taran Lynn <taranlynn0@gmail.com>
2017-08-24 20:53:30 -0700

    Added unit test for the `id` function.
    i.e. `\a : *. \x : a. x`.

--- test/CheckSpec.hs
+++ test/CheckSpec.hs
@@ -5,8 +5,9 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Check
-import ReprGen
 import Reduce
+import ReprGen
+import Representation
 import Util
 
 
@@ -15,3 +16,9 @@ spec = do
   describe "checkTerm" $ do
     it "reduction of type checked term produces normal form" $ property $ \t ->
       typeChecks t ==> (isInNormalForm (reduceTerm t))
+
+    -- I hate unit tests, but I should at least test this term.
+    it "\\a : *. \\x : a. x has type @a : *. @x : a. a" $
+      checkObject (Fun (Just "a") (C Star) (Fun (Just "x") (O (Var "a" 0)) (Var "x" 0))) Star
+      `shouldBe`
+      Right (O (Prod (Just "a") (C Star) (Prod (Just "x") (O (Var "a" 0)) (Var "a" 1))))
